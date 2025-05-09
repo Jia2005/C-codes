d6ca29550decff0cec04d154fd12335b753dfea9
Jia Harisinghani <143592429+Jia2005@users.noreply.github.com>
2025-05-10 01:27:55 +0530

    Update nCr.c

--- nCr.c
+++ nCr.c
@@ -1,34 +1,39 @@
 //to calculate nCr
- #include<stdio.h>
- #include<conio.h>
- unsigned long fact(int n)
- {
- unsigned long f;
- f=1;
- while(n!=0) 
- {
- f=f*n;
- n--;
- }
- return fact;
- }
- void main()
- {
- int n,r;
- unsigned long n1,n2,nCr;
- clrscr();
- printf(“\n”);
- scanf(“%d%d”,&n,&r);
- if(n>r)
- {
- n1=fact(n);
- n2=fact(r)*fact(n-r);
- nCr=n1/n2;
- printf(“Value of nCr is %ul”,nCr);
- }
- else
- {
- printf(“nCr of these 2 numbers is not possible \n”);
- }
- getch();
- }
+#include<stdio.h>
+#include<conio.h>
+
+unsigned long fact(int n)
+{
+    unsigned long f;
+    f = 1;
+    while(n != 0) 
+    {
+        f = f * n;
+        n--;
+    }
+    return f;  
+}
+
+void main()
+{
+    int n, r;
+    unsigned long n1, n2, nCr;
+    
+    clrscr();
+    printf("\nEnter the values of n and r: ");
+    scanf("%d%d", &n, &r);
+    
+    if(n >= r)
+    {
+        n1 = fact(n);
+        n2 = fact(r) * fact(n-r);
+        nCr = n1 / n2;
+        printf("Value of nCr is %lu", nCr);  // %lu for unsigned long
+    }
+    else
+    {
+        printf("nCr of these 2 numbers is not possible \n");
+    }
+    
+    getch();
+}
