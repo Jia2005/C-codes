851849647deed5cc33855495e37e8717391999f8
Jia Harisinghani <143592429+Jia2005@users.noreply.github.com>
2025-05-10 01:42:08 +0530

    Update Palindrome.c

--- Palindrome.c
+++ Palindrome.c
@@ -1,54 +1,42 @@
 #include<stdio.h>
-#include<conio.h>
+#include<conio.h> 
+#include<string.h>
 
 void main()
 {
-    int a[10][10], i, j, m, n, rowsum, colsum;
+    int i, j, len, flag=0; 
+    char a[50], reva[50];
     
     clrscr();
-    printf("Enter the dimensions of the matrix (rows columns):\n");
-    scanf("%d%d", &m, &n);
+    printf("\n Enter a string: ");    
+    gets(a);
+    puts(a);
     
-    printf("Enter the matrix elements:\n");
-    for(i=0; i<m; i++)
+    len = strlen(a);
+    for(i=len-1, j=0; i>=0; i--, j++)
+    { 
+        reva[j] = a[i];
+    }
+    reva[j] = '\0';    
+    puts(reva);
+    
+    for(i=0; i<len; i++)
     {
-        for(j=0; j<n; j++)
+        if(reva[i] != a[i])
         {
-            scanf("%d", &a[i][j]);
-        }
+            flag = 1;
+            break; 
+        } 
     }
     
-    printf("\nThe matrix is:\n");
-    for(i=0; i<m; i++)
+    if(flag == 0)
     {
-        for(j=0; j<n; j++)
-        {
-            printf("%d\t", a[i][j]);  // Added tab for better spacing
-        }
-        printf("\n");
+        printf("\n The given string is a palindrome");
+    }
+    else
+    {
+        printf("\n The given string is not a palindrome");
     }
     
-    printf("\nRow sums:\n");
-    for(i=0; i<m; i++)
-    {
-        rowsum = 0;
-        for(j=0; j<n; j++)
-        {
-            rowsum = rowsum + a[i][j];
-        }
-        printf("The sum of row %d is %d\n", i+1, rowsum);
-    }
-    
-    printf("\nColumn sums:\n");
-    for(j=0; j<n; j++)
-    {
-        colsum = 0;
-        for(i=0; i<m; i++)
-        {
-            colsum = colsum + a[i][j];
-        }
-        printf("The sum of column %d is %d\n", j+1, colsum);
-    }
-    
-    getch();
+    getch();    
 }
