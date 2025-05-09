c5d3f1995954379ad829bfbcf306ae6e30fd4333
Jia Harisinghani <143592429+Jia2005@users.noreply.github.com>
2025-05-10 01:47:02 +0530

    Update Armstrong.c

--- Armstrong.c
+++ Armstrong.c
@@ -1,34 +1,32 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
-{
-int i,n1,n2,d,sum,count,j,y;
-clrscr();
-for(i=100;i<=10000;i++)
-{
-n1=i;n2=i;
-sum=0;
-count=0;
-while(n1!=0)
-{
-n1=n2/10;
-count++;
-}
-while(n2!=0)
-{
- d=n2%10;
- for(j=1;j<=count;j++)
- {
-y=1;
-y=y*d;
- }
-n2=n2/10;
-sum=sum+y;
- }
-(sum==i);
-{
-printf(“%d \n”,i);
-}
-}
-getch();
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int i, n, digit, sum, temp, count;
+
+    for (i = 100; i <= 10000; i++) {
+        n = i;
+        count = 0;
+        temp = n;
+
+        while (temp != 0) {
+            temp = temp / 10;
+            count++;
+        }
+
+        sum = 0;
+        temp = n;
+
+        while (temp != 0) {
+            digit = temp % 10;
+            sum += pow(digit, count);
+            temp = temp / 10;
+        }
+
+        if (sum == i) {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
 }
