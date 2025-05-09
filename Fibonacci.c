07943cb1f76627367d3c623869bc8bad9d5d6dd2
Jia Harisinghani <143592429+Jia2005@users.noreply.github.com>
2025-05-10 01:43:12 +0530

    Update Fibonacci.c

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,34 +1,43 @@
 #include<stdio.h>
 #include<conio.h>
- void main()
- {
- int a,b,c,n,i,x,f;
- a=0,b=1;
- clrscr();
- printf(“Enter the range of Fibonacci series \n”);
- scanf(“%d”,&n);
- printf(“\n%d \n%d”,a,b);
- printf(“\n Enter the number”);
- scanf((“%d”,&x);
- for(i=3;i<=n;n++)
- { 
- c=a+b;
- printf(“\n%d”,c);
- a=b;
- b=c;
- if(x==c)
- {
- f=0;
- break;
- }
+
+void main()
+{
+    int a, b, c, n, i, x, f=1;  
+    a=0;
+    b=1;  
+    
+    clrscr();
+    printf("Enter the range of Fibonacci series \n");
+    scanf("%d", &n);
+    
+    printf("\n%d \n%d", a, b);
+    
+    printf("\nEnter the number to search: ");  
+    scanf("%d", &x);  
+    
+    for(i=3; i<=n; i++)  
+    {
+        c = a + b;
+        printf("\n%d", c);
+        a = b;
+        b = c;
+        
+        if(x == c)
+        {
+            f = 0;
+            break;
+        }
+    }
+    
+    if(f == 0)
+    {
+        printf("\nYes, it is a part of Fibonacci series");
+    }
+    else
+    {
+        printf("\nNo, it is not a part of Fibonacci series");
+    }
+    
+    getch();
 }
- if(f==0)
- {
- printf(“\nYes, it is a part of Fibonacci series”);
- }
- else
- {
- printf(“\nNo, it is not a part of Fibonacci series”); 
- }
- getch();
- }
